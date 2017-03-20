285973aa6bfc1096991f6e00e17f2cf9ab32d6a1
Wilmer Rafael Bandres Hernandez <wbandres@mahisoft.com>
2017-03-20 22:21:54 +0100

    fixing main

--- main.py
+++ main.py
@@ -6,16 +6,19 @@ import util
 import constants
 import bfs
 import log_filter
+import sys
 try:
     from cStringIO import StringIO
 except:
     from StringIO import StringIO
 
 if __name__ == "__main__":
-    for x in range(1,10):
+    f = open("out", "a")
+    for x in range(int(sys.argv[1]), 10):
         file_name = "logs/" + constants.log_name + str(x) + ".txt"
         out_file = "graph/" + constants.log_name + str(x) + ".txt"
         urls = log_filter.get_filtered_lines(file_name, out_file)
-        print "Urls ready..."
+        #print "Urls ready..."
         bfs.bfs(urls[1:])
+        f.write("Ready %s.\n" % str(x))
 
